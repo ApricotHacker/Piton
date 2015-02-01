871a47b5e43d77f57f53bf8c410e6424f27179f7
Barak Nehmad <bnehmad@gmail.com>
2015-02-01 19:43:52 +0200

    CR on assignment #1
    Great job!
    Summary in ending of code.
    Specifics in-code, labeled with CR.
    Next assignment comes to issues list right up!
    Remember to mark the issue as finished.

--- PyCalc/PyCalc.py
+++ PyCalc/PyCalc.py
@@ -20,6 +20,13 @@ elif operand=="/" :
 elif operand=="**" :
     print("%s **2 %s = %s") % (num1,num2, int(num1)**int(num2))
 
+# CR: Your program should be able to handle exceptions well. If I input a string (or anything which isn't an int)
+#     as one of the parameters, the program just exits without any exceptions.
+#     A try;except statement, which catches exceptions and allows for retries, would work well here!
+
+# CR: Perhaps implement here some kind of ReadKey function, so the user can see the result before the program exits.
+#     Maybe another raw_input but with no implications?
+
 ''' Sorry for the lack of PRO-IZM
 I know this code is stupid as fock '''
 
@@ -28,6 +35,9 @@ I know this code is stupid as fock '''
 # Assignment for : Barak Nehmad ( AKA @SandSpider2234 - GitHub )
 # Enter grade of assignment here :
 '''
-<Input your comments here>
+I shit you not, I've been trying to look for small mistakes here and there, but couldn't find anything except for two.
+This is an overall well designed code, which does what I asked you to do (and much, much more!!!)
+
+Grade: 95! Great job!
 '''
 
